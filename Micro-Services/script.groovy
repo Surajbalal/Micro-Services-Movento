75e21ee6747c7d81bd573207bed3796aa99701ce
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-08-26 21:28:24 +0530

    configure environment files for Jenkins tests

--- Micro-Services/script.groovy
+++ Micro-Services/script.groovy
@@ -1,4 +1,12 @@
 def test(){
+      withCredentials([
+        file(credentialsId: 'auth-service.env', variable: 'AUTH_ENV_FILE'),
+        file(credentialsId: 'user-service.env', variable: 'USER_ENV_FILE'),
+        file(credentialsId: 'captain-service.env', variable: 'CAPTAIN_ENV_FILE'),
+        file(credentialsId: 'ride-service.env', variable: 'RIDE_ENV_FILE'),
+        file(credentialsId: 'payment-service.env', variable: 'PAYMENT_ENV_FILE'),
+        file(credentialsId: 'call-service.env', variable: 'CALL_ENV_FILE')
+    ]) {
      sh ''' 
               cd Micro-Services
                 docker compose run --rm auth pnpm test
@@ -8,6 +16,7 @@ def test(){
             docker compose run --rm payment pnpm test
             docker compose run --rm call-service pnpm test
             '''
+    }
 }
 def buildImage(){
      sh '''
