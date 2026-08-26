7bc2b555b4f49c8ec61ce8dcfc786464df3c72c7
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-08-26 22:06:40 +0530

    remove test command in groovy

--- Micro-Services/script.groovy
+++ Micro-Services/script.groovy
@@ -1,12 +1,12 @@
-def test(){
-      withCredentials([
-        file(credentialsId: 'auth-service.env', variable: 'AUTH_ENV_FILE'),
-        file(credentialsId: 'user-service.env', variable: 'USER_ENV_FILE'),
-        file(credentialsId: 'captain-service.env', variable: 'CAPTAIN_ENV_FILE'),
-        file(credentialsId: 'ride-service.env', variable: 'RIDE_ENV_FILE'),
-        file(credentialsId: 'payment-service.env', variable: 'PAYMENT_ENV_FILE'),
-        file(credentialsId: 'call-service.env', variable: 'CALL_ENV_FILE')
-    ]) {
+// def test(){
+//       withCredentials([
+//         file(credentialsId: 'auth-service.env', variable: 'AUTH_ENV_FILE'),
+//         file(credentialsId: 'user-service.env', variable: 'USER_ENV_FILE'),
+//         file(credentialsId: 'captain-service.env', variable: 'CAPTAIN_ENV_FILE'),
+//         file(credentialsId: 'ride-service.env', variable: 'RIDE_ENV_FILE'),
+//         file(credentialsId: 'payment-service.env', variable: 'PAYMENT_ENV_FILE'),
+//         file(credentialsId: 'call-service.env', variable: 'CALL_ENV_FILE')
+//     ]) {
     //  sh ''' 
     //           cd Micro-Services
     //             docker compose run --rm auth pnpm test
@@ -17,6 +17,9 @@ def test(){
     //         docker compose run --rm call-service pnpm test
     //         '''
     // }
+// }
+def test() {
+    echo 'Tests temporarily disabled'
 }
 def buildImage(){
      sh '''
