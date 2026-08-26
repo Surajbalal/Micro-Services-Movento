7034821c5d3b60753a91640ac5582e6321831d3c
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-08-26 22:32:14 +0530

    add env credentials in jenkinsfile

--- Micro-Services/script.groovy
+++ Micro-Services/script.groovy
@@ -42,11 +42,19 @@ def dockerLogin(){
         '''
                 }
 }
-def pushImage(){
-       sh '''
-          cd Micro-Services
-       
-       docker compose push
-       '''
+def buildImage() {
+    withCredentials([
+        file(credentialsId: 'auth-service.env', variable: 'AUTH_ENV_FILE'),
+        file(credentialsId: 'user-service.env', variable: 'USER_ENV_FILE'),
+        file(credentialsId: 'captain-service.env', variable: 'CAPTAIN_ENV_FILE'),
+        file(credentialsId: 'ride-service.env', variable: 'RIDE_ENV_FILE'),
+        file(credentialsId: 'payment-service.env', variable: 'PAYMENT_ENV_FILE'),
+        file(credentialsId: 'call-service.env', variable: 'CALL_ENV_FILE')
+    ]) {
+        sh '''
+            cd Micro-Services
+            docker compose build
+        '''
+    }
 }
 return this
