cfb3d4d1037310e9af3d16925a45c1b343e2ea86
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-09-09 18:13:12 +0530

    test pipeline

--- Micro-Services/script.groovy
+++ Micro-Services/script.groovy
@@ -219,6 +219,7 @@ def pushVersionUpdate(){
    sh '''
    git config user.name "jenkins"
    git config user.email "surajbalal786@gmail.com"
+   git remote set-url origin https://github.com/Surajbalal/Micro-Services-Movento.git
    '''
 
    sshagent(['github-ssh']){
