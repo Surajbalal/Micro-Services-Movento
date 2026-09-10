1532fbd1285e7ce0bdc56a2a8a45d609bbb3347c
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-09-10 16:21:39 +0530

    chore: separate build and deployment compose configuration

--- Micro-Services/server-cmds.sh
+++ Micro-Services/server-cmds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker compose --env-file deploy.env pull
-docker compose --env-file deploy.env up -d
+docker compose -f docker-compose.deploy.yaml --env-file deploy.env pull
+docker compose -f docker-compose.deploy.yaml --env-file deploy.env up -d
