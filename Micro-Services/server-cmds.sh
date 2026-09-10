8b77991fff9963606ae84d636da83322f3bb086c
Surajbalal <118155637+Surajbalal@users.noreply.github.com>
2026-09-10 18:05:35 +0530

    chore: fix docker-compose extension

--- Micro-Services/server-cmds.sh
+++ Micro-Services/server-cmds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker compose -f docker-compose.deploy.yaml --env-file deploy.env pull
-docker compose -f docker-compose.deploy.yaml --env-file deploy.env up -d
+docker compose -f docker-compose.deploy.yml --env-file deploy.env pull
+docker compose -f docker-compose.deploy.yml --env-file deploy.env up -d
